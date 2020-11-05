0ed3f81bccba8eba4a676158a7a194c318327729
Chelsea <chelsea.e.rolland@gmail.com>
2020-11-04 23:16:55 -0600

    completed the bonuses from the loops prime directive project

--- src/PrimeDirective.java
+++ src/PrimeDirective.java
@@ -23,32 +23,50 @@ public class PrimeDirective {
         return true;
     }
 
-    public ArrayList<Integer> onlyPrimes (int [] numbers) {
+    public ArrayList<Integer> onlyPrimes(int[] numbers) {
         ArrayList<Integer> primes = new ArrayList<Integer>();
-        for (int number : numbers ) {
-            if (isPrime(number)){
+        for (int number : numbers) {
+            if (isPrime(number)) {
                 primes.add(number);
             }
         }
         return primes;
     }
 
-
-    public boolean isEvenOrOdd (int number){
-        if (number % 2 == 0){
-            //if even
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public ArrayList<Integer> filterEvenOrOdd (int [] numbers) {
+    public ArrayList<Integer> filterEven(int[] numbers) {
         ArrayList<Integer> selectedNumbers = new ArrayList<Integer>();
         for (int num : numbers) {
-            if (isEvenOrOdd(num)) {
+            if (num % 2 == 0) {
                 selectedNumbers.add(num);
             }
+        }
+        return selectedNumbers;
+    }
+
+    public ArrayList<Integer> filterOdd(int[] numbers) {
+        ArrayList<Integer> selectedNumbers = new ArrayList<Integer>();
+        for (int num : numbers) {
+            if (num % 2 != 0) {
+                selectedNumbers.add(num);
+            }
+        }
+        return selectedNumbers;
+    }
+
+    public int firstPrime(int[] numbers) {
+        return onlyPrimes(numbers).get(0);
+    }
+
+    public ArrayList<Integer> fibonacciSequence(int[] numbers) {
+        ArrayList<Integer> selectedNumbers = new ArrayList<Integer>();
+
+        int t1 = numbers[0], t2 = numbers[1];
+        for (int i = 1; i <= numbers.length; ++i) {
+            selectedNumbers.add(t1);
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
         }
         return selectedNumbers;
     }
@@ -63,6 +81,9 @@ public class PrimeDirective {
         System.out.println("pd.isPrime(0) = " + pd.isPrime(0));
 
         System.out.println("pd.onlyPrimes(numbers) = " + pd.onlyPrimes(numbers));
-        System.out.println("pd.filterEitherOddOrEven(numbers) = " + pd.filterEvenOrOdd(numbers));
+        System.out.println("pd.filterEven(numbers) = " + pd.filterEven(numbers));
+        System.out.println("pd.filterOdd(numbers) = " + pd.filterOdd(numbers));
+        System.out.println("pd.firstPrime(numbers) = " + pd.firstPrime(numbers));
+        System.out.println("pd.fibonacciSequence(numbers) = " + pd.fibonacciSequence(numbers));
     }
 }
